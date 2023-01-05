add76a9e9687b033a64abb71a2857ed90b352814
derex <derex@outlook.com.br>
2023-01-05 03:50:04 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-nw.Window.open('./src/index.html', {}, function(win) {});
+nw.Window.open('./src/index.html', {}, function (win) { 
+    win.setMaximumSize(801, 501)
+    win.setMinimumSize(400, 200)
+    win.setPosition("center")
+});
